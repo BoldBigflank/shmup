13ba53d8e118c84aa0737657db56fdd2a4604808
Alex Swan <alex@bold-it.com>
2015-01-28 01:07:42 -0800

    Double the scale, use new Tilemap importer

---
+++ Assets/Resources/Default.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Default" tilewidth="32" tileheight="32">
+ <image source="../../../shmup stuff/tilemap.png" width="128" height="128"/>
+ <tile id="0">
+  <properties>
+   <property name="Name" value="blank"/>
+  </properties>
+ </tile>
+</tileset>
