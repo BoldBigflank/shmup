6694c3ef796fe401f59c7d0b40d9ce82a64a6d04
Alex Swan <alex@bold-it.com>
2015-02-28 11:21:20 -0800

    enemyscript changes

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -2,19 +2,47 @@
 using System.Collections;
 
 public class EnemyScript : MonoBehaviour {
+	Vector2 lastKnowntargetPosition;
+	public LayerMask layerMask;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    
+		// Cast a ray at each Player
+		float distance = 30.0F;
 
-        // Cast a ray at every Player
+		foreach(GameObject playerObject in GameObject.FindGameObjectsWithTag ("Player")){
+//			int layerMask = LayerMask.NameToLayer("Foreground") | LayerMask.NameToLayer("Player");
 
-        // If visible, go forward and turn toward the last place we saw them
+			Debug.DrawRay(transform.position, transform.TransformDirection(playerObject.transform.position-transform.position), Color.red);
+			RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(playerObject.transform.position-transform.position), distance*2.0F, layerMask);
+			if (hit.collider != null) {
+				Debug.Log ("Found something " + hit.collider.tag);
+				Debug.DrawLine(hit.point-Vector2.one, hit.point+Vector2.one, Color.white);
+//				Debug.DrawLine(hit.point-Vector2.one, hit.point+Vector2.one, Color.white);
+				// Found something
+				if(hit.collider.gameObject.CompareTag("Player")){
+					Debug.Log ("Found the player");
+					lastKnowntargetPosition = hit.point;
+
+				}
+//				transform.position = hit.point;
+			}
+		}
+
+		GoToLastKnownPosition();
+	}
+
+	void GoToLastKnownPosition(){
+		// If we're close enough, return
+//		transform.LookAt(lastKnowntargetPosition);
+
+		// Rotate toward the point
+
+		// Move forward
 
 	}
 	
