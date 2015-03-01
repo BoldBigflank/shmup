ecb70e80789d4700d23855499238f7e35a0752d5
Alex Swan <alex@bold-it.com>
2015-02-28 23:59:56 -0800

    enemies go to the last spot they saw a target

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -2,11 +2,16 @@
 using System.Collections;
 
 public class EnemyScript : MonoBehaviour {
-	Vector2 lastKnowntargetPosition;
+	Vector2 lastKnownTargetPosition;
 	public LayerMask layerMask;
+    public float RotationSpeed;
+
+    Quaternion lookRotation;
+    Vector3 direction;
 
 	// Use this for initialization
 	void Start () {
+        lastKnownTargetPosition = (Vector2)transform.position;
 	}
 	
 	// Update is called once per frame
@@ -17,8 +22,9 @@ public class EnemyScript : MonoBehaviour {
 		foreach(GameObject playerObject in GameObject.FindGameObjectsWithTag ("Player")){
 //			int layerMask = LayerMask.NameToLayer("Foreground") | LayerMask.NameToLayer("Player");
 
-			Debug.DrawRay(transform.position, transform.TransformDirection(playerObject.transform.position-transform.position), Color.red);
-			RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(playerObject.transform.position-transform.position), distance*2.0F, layerMask);
+            Debug.DrawRay(transform.position, (playerObject.transform.position - transform.position), Color.red);
+			// RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(playerObject.transform.position-transform.position), distance*2.0F, layerMask);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, (playerObject.transform.position - transform.position) , distance * 2.0F, layerMask);
 			if (hit.collider != null) {
 				Debug.Log ("Found something " + hit.collider.tag);
 				Debug.DrawLine(hit.point-Vector2.one, hit.point+Vector2.one, Color.white);
@@ -26,24 +32,34 @@ public class EnemyScript : MonoBehaviour {
 				// Found something
 				if(hit.collider.gameObject.CompareTag("Player")){
 					Debug.Log ("Found the player");
-					lastKnowntargetPosition = hit.point;
+					lastKnownTargetPosition = hit.point;
 
+                    break;
 				}
-//				transform.position = hit.point;
 			}
 		}
-
-		GoToLastKnownPosition();
 	}
+
+    void FixedUpdate()
+    {
+        GoToLastKnownPosition();
+    }
 
 	void GoToLastKnownPosition(){
 		// If we're close enough, return
-//		transform.LookAt(lastKnowntargetPosition);
+        if (((Vector2)transform.position - lastKnownTargetPosition).magnitude < 1.0F) { rigidbody2D.velocity = Vector2.zero; return; };
+//		transform.LookAt(lastKnownTargetPosition);
+        direction = (lastKnownTargetPosition - (Vector2)transform.position).normalized;
+
+        lookRotation = Quaternion.LookRotation(direction);
 
 		// Rotate toward the point
-
+        Quaternion q = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * RotationSpeed);
+        rigidbody2D.MoveRotation(q.eulerAngles.z);
 		// Move forward
-
+        // rigidbody2D.AddRelativeForce(Vector3.left, ForceMode2D.Impulse);
+        rigidbody2D.velocity = Vector2.zero;
+        rigidbody2D.AddForce(direction * 8.0F, ForceMode2D.Impulse);
 	}
 	
 	void OnCollisionEnter2D(Collision2D other){
